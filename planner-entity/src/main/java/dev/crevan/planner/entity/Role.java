a605837cd6e903d6d3a7cf504b05b60e27a7ff33
Crevan <a.siverskii@gmail.com>
2024-01-17 19:45:39 +0300

    [update]:[entity]: model of User and Role

--- planner-entity/src/main/java/dev/crevan/planner/entity/Role.java
+++ planner-entity/src/main/java/dev/crevan/planner/entity/Role.java
@@ -1,5 +1,6 @@
 package dev.crevan.planner.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "role_data", schema = "todoList", catalog = "postgres")
@@ -22,12 +24,12 @@ public class Role {
 
     private String name;
 
-/*    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "role_id"),
-    inverseJoinColumns = @JoinColumn(name = ))
-    private Set<User> users; //TODO
-*/
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> users;
 
     @Override
     public int hashCode() {
